adf05ede967b2220356961bbd4c32330b3913f71
AyushInKC <sirfayush1008@gmail.com>
2024-12-20 23:37:25 +0530

    Project Setup

---
+++ src/test/java/com/SpringProject/CallingApp/CallingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringProject.CallingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
